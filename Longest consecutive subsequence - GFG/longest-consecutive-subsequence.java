7ac8cb3f3233206bb567171deb1818bd278f9b8b
samarthlawania <93425520+samarthlawania@users.noreply.github.com>
2023-02-01 12:33:52 +0530

    Added solution - LeetHub

---
+++ Longest consecutive subsequence - GFG/longest-consecutive-subsequence.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			
+			for(int i=0; i<n; i++)
+				a[i] = sc.nextInt();
+		    out.println(new Solution().findLongestConseqSubseq(a, n));
+		    t--;
+		}
+		out.flush();
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{   
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+	static int findLongestConseqSubseq(int arr[], int n)
+	{
+	   int freq[] = new int[100001];
+	   int c = 0,max=0;
+	   for(int i =0;i<n;i++){
+	       freq[arr[i]]++;
+	   }
+	   for(int i=0;i<freq.length;i++){
+	       if(freq[i]!=0) c++;
+	       if(c>max) max = c;
+	       if(freq[i]==0) c=0;
+	   }
+	   return max;
+	}
+}
