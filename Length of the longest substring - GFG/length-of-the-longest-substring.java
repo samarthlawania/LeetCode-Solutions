74961a29373f4f4d456b32e0e4cb1703780a0d7a
samarthlawania <93425520+samarthlawania@users.noreply.github.com>
2023-02-05 09:55:42 +0530

    Added solution - LeetHub

--- Length of the longest substring - GFG/length-of-the-longest-substring.java
+++ Length of the longest substring - GFG/length-of-the-longest-substring.java
@@ -32,31 +32,16 @@ class Solution{
             String c = str.charAt(e)+"";
             if(!(st.contains(c))) {
                 l = e-s+1;
-              //  st = str.substring(s,e+1);
             }
             if(s<=e&&st.contains(c)){
                 s=st.length()-st.indexOf(c)-1;
                 st=st.substring(st.indexOf(c)+1);
             }
-       //     System.out.println("e"+e+"s"+s);
        st = st+c;
-        //    System.out.println(st);
             l = st.length();
             max = Math.max(l,max);
             e++;
-        //    System.out.println(st);
-           // if(e<str.length()-1)
         }
         return max;
     }
- /*   static boolean frq(String str,int s,int e){
-        int frq[] = new int[26];
-        for(int i=s;i<=e;i++){
-            frq[str.charAt(i)-'a']++;
-        }
-        for(int i=0;i<26;i++){
-            if(frq[i]>1) return false;
-        }
-        return true;
-    } */
 }
