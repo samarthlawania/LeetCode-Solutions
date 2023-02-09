15bdc3d857379988e9d33c76115d845f62857045
samarthlawania <93425520+samarthlawania@users.noreply.github.com>
2023-02-09 11:09:22 +0530

    Added solution - LeetHub

---
+++ Quick Left Rotation - GFG/quick-left-rotation.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+class Solution
+{
+    void leftRotate(long arr[], int k,int n)
+    {
+        if(k>=n){
+            k=k%n;
+        }
+        Reverse(arr,k,n-1);
+        Reverse(arr,0,k-1);
+        Reverse(arr,0,n-1);
+    }
+    void Reverse(long[] arr,int i,int j){
+        while(i<j){
+            long temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+//            int n = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] q = line.trim().split("\\s+");
+            int n =Integer.parseInt(q[0]);
+            int k =Integer.parseInt(q[1]);
+//            //int y =Integer.parseInt(a2[2]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(a1[i]);
+            }
+//            String line2 = br.readLine();
+//            String[] a2 = line2.trim().split("\\s+");
+//            long b[] = new long[n];
+//            for (int i = 0; i < m; i++) {
+//                b[i] = Long.parseLong(a2[i]);
+//            }
+            Solution ob = new Solution();
+            //ArrayList<Long> ans=ob.smallestDifferenceTriplet(a,b,c,n);
+            ob.leftRotate(a,k,n);
+            for (int i = 0; i < n; i++)
+                System.out.print(a[i]+" ");
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+// } Driver Code Ends
