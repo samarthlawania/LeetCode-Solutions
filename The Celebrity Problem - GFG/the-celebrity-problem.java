7e18a0ecd08e46daafecd1d861c7caf72803acd0
samarthlawania <93425520+samarthlawania@users.noreply.github.com>
2023-03-25 12:08:16 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.java
+++ The Celebrity Problem - GFG/the-celebrity-problem.java
@@ -33,7 +33,7 @@ class GFG{
 class Solution
 { 
     //Function to find if there is a celebrity in the party or not.
-    int celebrity(int M[][], int n)
+   int celebrity(int M[][], int n)
     {
         int c=0,c1=0,ans=0;;
     	for(int i=0;i<n;i++){
@@ -49,6 +49,6 @@ class Solution
     	    }
     	}
     	if(c1==1) return ans;
-    	else return -1;
+    	 return -1;
     }
 }
